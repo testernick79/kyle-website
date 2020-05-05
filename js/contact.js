ef39c8ec9ca24858904957a55a8cca19b8fd0b34
nick <123@gmail.com>
2020-05-05 12:47:47 -0700

    updated contact page, home page with button linking to a vendors site in a new tab

--- js/contact.js
+++
@@ -1,8 +0,0 @@
-// Listen to form
-document.getElementById( 'contactForm' ).addEventListener( 'submit', submitForm );
-
-function submitForm( e ) {
-    e.preventDefault();
-
-    console.log( 111 );
-}
